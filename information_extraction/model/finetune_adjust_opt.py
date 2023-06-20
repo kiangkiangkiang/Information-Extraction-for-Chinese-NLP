ce7dfc2543c479b0d07dbe069b899c452db40a5a
Bo Syue  Jiang <lukajiang1998@gmail.com>
2023-06-20 13:58:14 +0800

    a

--- information_extraction/model/finetune_adjust_opt.py
+++ information_extraction/model/finetune_adjust_opt.py
@@ -206,6 +206,7 @@ def finetune(
         else optimizers[0]
     )
     """
+    breakpoint()
     trainer.optimizers = (
         optimizer.RMSProp(learning_rate=training_args.learning_rate, parameters=model.parameters())
         if optimizers is None
