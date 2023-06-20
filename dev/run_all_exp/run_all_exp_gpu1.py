4b0a4a3f08366332d820677f0dde758786ab5dc9
Bo Syue  Jiang <lukajiang1998@gmail.com>
2023-06-20 17:03:01 +0800

    run

--- dev/run_all_exp/run_all_exp_gpu1.py
+++ dev/run_all_exp/run_all_exp_gpu1.py
@@ -39,27 +39,27 @@ loss_weights = ["1 1.1184 1.7745", "1 2 4", "1 1.5 2", "1 3 6", "1 2 3"]
 for lw in loss_weights:
     os.system(
         f"python3 ../../information_extraction/model/finetune_loss_by_group.py  \
-        --device gpu:0 \
+        --device gpu:1 \
         --logging_steps 10 \
-        --save_steps 600 \
-        --eval_steps 600 \
+        --save_steps 750 \
+        --eval_steps 750 \
         --seed 1000 \
         --model_name_or_path uie-base  \
-        --train_path ../../information_extraction/data/toy_data/training_data.txt \
-        --dev_path ../../information_extraction/data/toy_data/eval_data.txt  \
-        --test_path ../../information_extraction/data/toy_data/testing_data.txt  \
+        --train_path ../../information_extraction/data/final_data/training_data.txt \
+        --dev_path ../../information_extraction/data/final_data/eval_data.txt  \
+        --test_path ../../information_extraction/data/final_data/testing_data.txt  \
         --max_seq_len 768  \
         --read_data_method chunk \
         --per_device_eval_batch_size 8 \
         --per_device_train_batch_size 8 \
         --multilingual True \
-        --num_train_epochs 1 \
+        --num_train_epochs 4 \
         --learning_rate 1.25e-05 \
         --label_names 'start_positions' 'end_positions' \
         --do_train \
         --do_eval \
         --do_export \
-        --output_dir ../../information_extraction/results/test_ckp_final_data_768_epochs4_lr1.25e-05_loss{lw.replace(' ', '-')}\
+        --output_dir ../../information_extraction/results/ckp_final_data_768_epochs4_lr1.25e-05_loss{lw.replace(' ', '-')}\
         --overwrite_output_dir \
         --disable_tqdm True \
         --metric_for_best_model eval_f1 \
