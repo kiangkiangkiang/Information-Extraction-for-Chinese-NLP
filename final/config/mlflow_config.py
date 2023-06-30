7f34ef01803e2cf22d750aae38f8d2f3765a6b6c
Bo Syue  Jiang <lukajiang1998@gmail.com>
2023-06-30 17:30:45 +0800

    aaa

--- final/config/mlflow_config.py
+++ final/config/mlflow_config.py
@@ -1,6 +1,6 @@
 # Add MLflow for experiment # change mlflow to False
 MLFLOW = False
 is_experiment = True
-os.environ["MLFLOW_TRACKING_URI"] = "http://ec2-44-213-176-187.compute-1.amazonaws.com:7003"
+os.environ["MLFLOW_TRACKING_URI"] = ""
 os.environ["MLFLOW_TRACKING_USERNAME"] = "luka"
 os.environ["MLFLOW_TRACKING_PASSWORD"] = "luka"
